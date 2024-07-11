3d2f2f2e7e6ad4163404e4732c6d9698b00b9c32
Haruna Oseni <harunaoseni23@gmail.com>
2024-07-11 15:30:26 -0400

    chore: Add whitenoise middleware for static file serving

--- multicast/settings/__init__.py
+++ multicast/settings/__init__.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
