42305191b195572391bb316e1cdb526bd90248ac
Haruna Oseni <harunaoseni23@gmail.com>
2024-07-22 11:10:23 -0400

    Update timeout for FFprobe in start_ffmpeg function

--- multicast/apps/view/tasks.py
+++ multicast/apps/view/tasks.py
@@ -172,7 +172,7 @@ def start_ffmpeg(tunnel_id):
                 stdout=log,
                 stderr=subprocess.STDOUT,
                 check=True,
-                timeout=30,
+                timeout=60,
             )
     except subprocess.TimeoutExpired:
         logger.error(f"FFprobe timed out for tunnel {tunnel_id}")
