84f0b4ea1936cb2cbd120b3a380531e4416807da
Haruna Oseni <harunaoseni23@gmail.com>
2024-07-27 20:18:19 -0400

    chore: Update Celery broker pool limit to 10

--- multicast/celery.py
+++ multicast/celery.py
@@ -6,6 +6,7 @@ from celery import Celery
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "multicast.settings")
 
 app = Celery("multicast")
+app.conf.broker_pool_limit = 10
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
